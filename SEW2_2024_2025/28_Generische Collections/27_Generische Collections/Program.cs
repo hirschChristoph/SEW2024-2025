fa2c25a65561712dc9d02d4291744adc781f3359
hirschChristoph <hirschchristoph7@gmail.com>
2025-02-10 14:51:18 +0100

    new code

---
+++ SEW2_2024_2025/28_Generische Collections/27_Generische Collections/Program.cs
@@ -0,0 +1,16 @@
+﻿
+List<int> myList = new List<int>();
+
+Random random = new Random();
+myList.Add(random.Next());
+myList.Add(100);
+myList.Add(20);
+
+Console.WriteLine($"Elemente in Liste: {myList.Count}");
+Console.WriteLine($"Summe der Elemente: {myList.Sum()}");
+Console.WriteLine($"Zweites Element: {myList[1]}");
+
+foreach( int i in myList )
+{
+    Console.WriteLine(i);
+}
